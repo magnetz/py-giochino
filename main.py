e79cabaaf5667a79da7cbaee471b23238a82a749
Luca Magnetti <l.magnetti@NAXA.LOCAL>
2019-04-01 17:50:30 +0200

    Aggiunto menu iniziate e schermata di game over

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ from pygame.locals import *
 score = 0
 max_score = 0
 max_speed = 0
+start_menu = 1
+run = 1
+replay = 1
 
 def main():
-    global score, max_score, max_speed
+    global score, max_score, max_speed, run, replay, start_menu
 
     pygame.init()
     clock = pygame.time.Clock()
@@ -31,7 +34,33 @@ def main():
     po = point.Point(screen, 5, 5, 5, 5, (255, 255, 255))
     po.recollocate()
 
-    while 1:
+    while start_menu:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+        screen.fill(black)
+
+        start_myfont = pygame.font.SysFont('ubuntu', 30)
+        game_start = start_myfont.render('Giochino', False, white)
+        screen.blit(game_start, (10, 10))
+
+        text_myfont = pygame.font.SysFont('ubuntu', 20)
+        game_text = text_myfont.render('Press <SPACE> and play else press <ESC>', False, white)
+        screen.blit(game_text, (10, 40))
+
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]:
+            start_menu = 0
+            run = 1
+            main()
+
+        pygame.display.update()
+        clock.tick(60)
+
+    while run:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -44,21 +73,50 @@ def main():
         p.move()
 
         if p.wall_collision():
-            score = 0
-            screen.fill((255, 0, 0))
+            if score <= 0:
+                run = 0
+            else:
+                score = 0
+                screen.fill((255, 0, 0))
 
         collision_check(p, po)
 
-        score_text = myfont.render(('score: %(score)s Highscore: %(max_score)s' % {
-                                   "score": score, "max_score": max_score}), False, white)
-        speed_text = myfont.render(('Speed: %(speed)s Max speed: %(max_speed)s' % {
-                                   "speed": p.vel, "max_speed": max_speed}), False, white)
+        score_text = myfont.render(('score: %(score)s Highscore: %(max_score)s' % {"score": score, "max_score": max_score}), False, white)
+        speed_text = myfont.render(('Speed: %(speed)s Max speed: %(max_speed)s' % {"speed": p.vel, "max_speed": max_speed}), False, white)
         screen.blit(score_text, (10, 10))
         screen.blit(speed_text, (10, 20))
 
         pygame.display.update()
 
         clock.tick(60)
+
+    while replay:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+
+        screen.fill(black)
+
+        over_myfont = pygame.font.SysFont('ubuntu', 30)
+        game_over = over_myfont.render('Game Over!', False, white)
+        screen.blit(game_over, (10, 10))
+
+        text_myfont = pygame.font.SysFont('ubuntu', 20)
+        game_text = text_myfont.render('Reload with <SPACE> else press <ESC>', False, white)
+        screen.blit(game_text, (10, 40))
+
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]:
+            run = 1
+            main()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+    
 
 def collision_check(player, point):
     global score, max_score, max_speed
